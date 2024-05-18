9cc7e1baa4a8939547bcd361dc2b3c5e116d64c1
Manuchexra Nurmerojova <mnurmexrojova@gmail.com>
2024-05-18 21:29:41 +0500

    oooo

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';// Import your Redux store
 import './index.css';
-import { Provider} from 'react-redux';
+// import { Provider} from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
