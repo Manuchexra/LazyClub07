0750520dd35b60c55868ffb9c4cfba07c86ace1a
Manuchexra <135632769+Manuchexra@users.noreply.github.com>
2024-05-20 13:48:16 +0500

    Update views.py

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from .models import Register
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
 # Create your views here.
+
+class RegisterView(APIView):
+    def get(self,request):
+        output=[{
+            "first_name":output.first_name,
+            "last_name":output.last_name,
+            "age":output.age,
+            "email":output.email,
+            "username":output.username,
+            "password":output.password
+        }for output in Register.objects.all()]
+        return Response(output)
+    def post(self,request):
+        serializer=RegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
