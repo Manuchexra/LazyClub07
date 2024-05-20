d99fb17017ee7bfe8a5421169a8e01bbd0e332fb
Manuchexra Nurmerojova <mnurmexrojova@gmail.com>
2024-05-20 13:33:51 +0500

    Manuchexra

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,38 +1,38 @@
-import "./App.css";
-import axios from "axios";
-import React from "react";
-class App extends React.Component{
-    state={details:[],}
-    componentDidMount(){
-        let data;
-        axios.get("http://localhost:8000")
-        .then(res=>{
-            data=res.data;
-            this.setState({
-                details:data
-            });
-        })
+// import "./App.css";
+// import axios from "axios";
+// import React from "react";
+// class App extends React.Component{
+//     state={details:[],}
+//     componentDidMount(){
+//         let data;
+//         axios.get("http://localhost:8000")
+//         .then(res=>{
+//             data=res.data;
+//             this.setState({
+//                 details:data
+//             });
+//         })
 
-        .catch(err =>{
-            console.log(err);
-        })
-    }
-    render(){
-        return(
-            <div>
-                <header>Django</header>
-                <hr></hr>
-                {this.state.details.map((output,id)=>(
-                    <div key={id}>
-                        <div>
-                            <h2>output.username</h2>
-                            <p>output.password</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        )
-    }
-}
+//         .catch(err =>{
+//             console.log(err);
+//         })
+//     }
+//     render(){
+//         return(
+//             <div>
+//                 <header>Django</header>
+//                 <hr></hr>
+//                 {this.state.details.map((output,id)=>(
+//                     <div key={id}>
+//                         <div>
+//                             <h2>output.username</h2>
+//                             <p>output.password</p>
+//                         </div>
+//                     </div>
+//                 ))}
+//             </div>
+//         )
+//     }
+// }
 
-export default App;
+// export default App;
